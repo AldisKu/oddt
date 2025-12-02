fe2a3771a7ac7c81026d20111ccfd144bb12c747
root <root@cafekomine.de>
2025-12-02 09:58:29 +0000

    Original plugin

---
+++ block/assets/dist/pickup-fields-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-blocks-data-store', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ab8b9888dbd3f890b9d7');
